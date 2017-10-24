d5f50257f0ce81690d3e716717b8b3a70c0d75b9
Vadim Geyfman <vadim.geyfman@thomsonreuters.com>
2017-10-23 21:01:12 -0400

    Adde static file monitoring

--- gulpfile.js
+++ gulpfile.js
@@ -15,6 +15,8 @@ var jsSources = [
 	];
 
 var sassSources = ['components/sass/style.scss'];
+var htmlSources = ['builds/development/*.html'];
+var jsonSources = ['builds/development/js/*.json'];
 
 gulp.task('coffee', function(){
 	gulp.src(coffeeSources)
@@ -50,6 +52,8 @@ gulp.task('watch', function(){
 	gulp.watch(coffeeSources, ['coffee']);
 	gulp.watch(jsSources, ['js']);
 	gulp.watch('components/sass/*.scss', ['compass']);
+	gulp.watch(htmlSources, ['html']);
+	gulp.watch(jsonSources, ['json']);
 });
 
 gulp.task('connect', function(){
@@ -57,4 +61,14 @@ gulp.task('connect', function(){
 	    root: 'builds/development/',
 	    livereload: true
 	  });
+});
+
+gulp.task('html', function(){
+	gulp.src(htmlSources)
+	.pipe(connect.reload())
+});
+
+gulp.task('json', function(){
+	gulp.src(jsonSources)
+	.pipe(connect.reload())
 });
